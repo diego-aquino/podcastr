660eb249945fd1021582563d8b6735540345df42
Diego Aquino <diego.cruz001@gmail.com>
2021-04-20 10:02:16 -0300

    Update home to list the titles of mock episodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,32 @@
+import { GetStaticProps } from 'next';
 import { FC } from 'react';
 
-const Home: FC = () => <div>Hello world!</div>;
+import api from '~/api';
+
+interface EpisodeData {
+  id: string;
+  title: string;
+}
+
+interface PageProps {
+  episodes: EpisodeData[];
+}
+
+const Home: FC<PageProps> = ({ episodes }) => (
+  <div>
+    {episodes.map((episode) => (
+      <p key={episode.id}>{episode.title}</p>
+    ))}
+  </div>
+);
+
+export const getStaticProps: GetStaticProps<PageProps> = async () => {
+  const { data: episodes } = await api.get('/episodes');
+
+  return {
+    props: { episodes },
+    revalidate: 60 * 60 * 8,
+  };
+};
 
 export default Home;
